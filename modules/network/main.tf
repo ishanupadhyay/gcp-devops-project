68293e198f58382ecf56d125326568aec840e61f
ishan412 <i.upadhyay2@newcastle.ac.uk>
2026-05-09 15:14:18 +0530

    updated firewall rules

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -13,3 +13,30 @@ resource "google_compute_subnetwork" "subnet" {
   private_ip_google_access = false
 }
 
+resource "google_compute_firewall" "allow_ssh" {
+  name    = "${var.network_name}-allow-ssh"
+  network = google_compute_network.vpc.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+
+  target_tags = ["web-server"]
+}
+
+resource "google_compute_firewall" "allow_http" {
+  name    = "${var.network_name}-allow-http"
+  network = google_compute_network.vpc.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+
+  target_tags = ["web-server"]
+}
